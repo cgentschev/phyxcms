d96f219832c0314edbaeb937eee7648daefe7148
Constantine D. Gentschev <phyxdesign@gmail.com>
2018-06-27 18:28:58 +0300

    more debugging

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -120,7 +120,7 @@ class InstallCommand extends Command
 
     protected function renameThemeDirectory()
     {
-        rename(resource_path('views/themes/default'), resource_path('views/themes/' . config('phyxcms.theme_path')));
+        rename(resource_path('views\\themes\\default'), resource_path('views\\themes\\' . config('phyxcms.theme_name')));
     }
 
     /**
